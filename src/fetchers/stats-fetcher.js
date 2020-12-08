3f71862f0632618e19d1168b6bf9613beeef1432
SuperFola <lexplt.dev@gmail.com>
2020-12-08 20:57:39 +0100

    working on fixing the repo/star count

--- src/fetchers/stats-fetcher.js
+++ src/fetchers/stats-fetcher.js
@@ -7,9 +7,8 @@ const githubUsernameRegex = require("github-username-regex");
 require("dotenv").config();
 
 const fetcher = (variables, token) => {
-  return request(
-    {
-      query: `
+    return request({
+        query: `
       query userInfo($login: String!) {
         user(login: $login) {
           name
@@ -30,124 +29,120 @@ const fetcher = (variables, token) => {
           followers {
             totalCount
           }
-          repositories(first: 100, ownerAffiliations: OWNER, orderBy: {direction: DESC, field: STARGAZERS}) {
+          repositories(first: 500, ownerAffiliations: OWNER, orderBy: {direction: DESC, field: STARGAZERS}) {
             totalCount
             nodes {
-              stargazers {
-                totalCount
-              }
+              stargazerCount
             }
           }
         }
       }
       `,
-      variables,
-    },
-    {
-      Authorization: `bearer ${token}`,
-    },
-  );
+        variables,
+    }, {
+        Authorization: `bearer ${token}`,
+    }, );
 };
 
 // https://github.com/anuraghazra/github-readme-stats/issues/92#issuecomment-661026467
 // https://github.com/anuraghazra/github-readme-stats/pull/211/
-const totalCommitsFetcher = async (username) => {
-  if (!githubUsernameRegex.test(username)) {
-    logger.log("Invalid username");
-    return 0;
-  }
-
-  // https://developer.github.com/v3/search/#search-commits
-  const fetchTotalCommits = (variables, token) => {
-    return axios({
-      method: "get",
-      url: `https://api.github.com/search/commits?q=author:${variables.login}`,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/vnd.github.cloak-preview",
-        Authorization: `bearer ${token}`,
-      },
-    });
-  };
-
-  try {
-    let res = await retryer(fetchTotalCommits, { login: username });
-    if (res.data.total_count) {
-      return res.data.total_count;
+const totalCommitsFetcher = async(username) => {
+    if (!githubUsernameRegex.test(username)) {
+        logger.log("Invalid username");
+        return 0;
     }
-  } catch (err) {
-    logger.log(err);
-    // just return 0 if there is something wrong so that
-    // we don't break the whole app
-    return 0;
-  }
+
+    // https://developer.github.com/v3/search/#search-commits
+    const fetchTotalCommits = (variables, token) => {
+        return axios({
+            method: "get",
+            url: `https://api.github.com/search/commits?q=author:${variables.login}`,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/vnd.github.cloak-preview",
+                Authorization: `bearer ${token}`,
+            },
+        });
+    };
+
+    try {
+        let res = await retryer(fetchTotalCommits, { login: username });
+        if (res.data.total_count) {
+            return res.data.total_count;
+        }
+    } catch (err) {
+        logger.log(err);
+        // just return 0 if there is something wrong so that
+        // we don't break the whole app
+        return 0;
+    }
 };
 
 async function fetchStats(
-  username,
-  count_private = false,
-  include_all_commits = false,
+    username,
+    count_private = false,
+    include_all_commits = false,
 ) {
-  if (!username) throw Error("Invalid username");
+    if (!username) throw Error("Invalid username");
 
-  const stats = {
-    name: "",
-    totalPRs: 0,
-    totalCommits: 0,
-    totalIssues: 0,
-    totalStars: 0,
-    contributedTo: 0,
-    rank: { level: "C", score: 0 },
-  };
+    const stats = {
+        name: "",
+        totalPRs: 0,
+        totalCommits: 0,
+        totalIssues: 0,
+        totalStars: 0,
+        contributedTo: 0,
+        rank: { level: "C", score: 0 },
+    };
 
-  let res = await retryer(fetcher, { login: username });
+    let res = await retryer(fetcher, { login: username });
 
-  if (res.data.errors) {
-    logger.error(res.data.errors);
-    throw new CustomError(
-      res.data.errors[0].message || "Could not fetch user",
-      CustomError.USER_NOT_FOUND,
-    );
-  }
+    if (res.data.errors) {
+        logger.error(res.data.errors);
+        throw new CustomError(
+            res.data.errors[0].message || "Could not fetch user",
+            CustomError.USER_NOT_FOUND,
+        );
+    }
 
-  const user = res.data.data.user;
+    const user = res.data.data.user;
 
-  stats.name = user.name || user.login;
-  stats.totalIssues = user.issues.totalCount;
+    stats.name = user.name || user.login;
+    stats.totalIssues = user.issues.totalCount;
 
-  // normal commits
-  stats.totalCommits = user.contributionsCollection.totalCommitContributions;
+    // normal commits
+    stats.totalCommits = user.contributionsCollection.totalCommitContributions;
 
-  // if include_all_commits then just get that,
-  // since totalCommitsFetcher already sends totalCommits no need to +=
-  if (include_all_commits) {
-    stats.totalCommits = await totalCommitsFetcher(username);
-  }
+    // if include_all_commits then just get that,
+    // since totalCommitsFetcher already sends totalCommits no need to +=
+    if (include_all_commits) {
+        stats.totalCommits = await totalCommitsFetcher(username);
+    }
 
-  // if count_private then add private commits to totalCommits so far.
-  if (count_private) {
-    stats.totalCommits +=
-      user.contributionsCollection.restrictedContributionsCount;
-  }
+    // if count_private then add private commits to totalCommits so far.
+    if (count_private) {
+        stats.totalCommits +=
+            user.contributionsCollection.restrictedContributionsCount;
+    }
 
-  stats.totalPRs = user.pullRequests.totalCount;
-  stats.contributedTo = user.repositoriesContributedTo.totalCount;
+    stats.totalPRs = user.pullRequests.totalCount;
+    stats.contributedTo = user.repositoriesContributedTo.totalCount;
 
-  stats.totalStars = user.repositories.nodes.reduce((prev, curr) => {
-    return prev + curr.stargazers.totalCount;
-  }, 0);
+    stats.totalStars = user.repositories.nodes.reduce((prev, curr) => {
+        return prev + curr.stargazerCount;
+    }, 0);
 
-  stats.rank = calculateRank({
-    totalCommits: stats.totalCommits,
-    totalRepos: user.repositories.totalCount,
-    followers: user.followers.totalCount,
-    contributions: stats.contributedTo,
-    stargazers: stats.totalStars,
-    prs: stats.totalPRs,
-    issues: stats.totalIssues,
-  });
+    stats.rank = calculateRank({
+        totalCommits: stats.totalCommits,
+        totalRepos: user.repositories.totalCount,
+        followers: user.followers.totalCount,
+        contributions: stats.contributedTo,
+        stargazers: stats.totalStars,
+        prs: stats.totalPRs,
+        issues: stats.totalIssues,
+    });
 
-  return stats;
+    return stats;
 }
 
 module.exports = fetchStats;
